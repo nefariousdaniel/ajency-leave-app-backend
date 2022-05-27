e7e2cae549ba853677fef4a08cc4c2f084b35d06
nefariousdaniel <99465571+nefariousdaniel@users.noreply.github.com>
2022-05-27 09:41:15 +0530

    Added Email Interation for leave request and leave approval

--- test.js
+++ test.js
@@ -1,3 +1,29 @@
-const bcrypt = require("bcrypt");
+const Airtable = require("airtable");
+const res = require("express/lib/response");
+const dotenv = require("dotenv").config()
 
-console.log(bcrypt.hashSync("password",8));
+
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.AIRTABLE_API_KEY
+});
+
+
+var base = Airtable.base(process.env.AIRTABLE_BASE)
+
+exports.handler = async function(event) {
+    var teamsBelongsTo = [];
+
+    
+    let result = await base("Company").find("recRWGW3MKU1L6d20")
+    teamsBelongsTo = result.fields["Teams (Belongs To)"];
+
+    teamsBelongsTo.forEach(async el=>{
+        result = await base("Company").select({filterByFormula:`FIND("${el}",{Teams Leading (Record ID)})`}).firstPage();
+        let to = result[0]._rawJson.fields.Email;
+        //Send Email from here using
+    });
+
+
+}
+this.handler()
